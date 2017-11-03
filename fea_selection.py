e30acc444f5ac583c7878959f2beaed36ae16522
Ahmed Elbagoury <am.elbagoury@gmail.com>
2017-11-03 19:54:37 -0400

    modify Feature selection

--- fea_selection.py
+++ fea_selection.py
@@ -2,12 +2,22 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import SelectFromModel
+from helper import load_data
 
-def univariate_fea_selection(X, y):
-	return SelectKBest(chi2, k=2).fit_transform(X, y)
+def univariate_fea_selection(X, y, k):
+	return SelectKBest(chi2, k=k).fit_transform(X, y)
 
-def SelectFromModel(X, y, sparsity_param):
+def my_SelectFromModel(X, y, sparsity_param):
 	lsvc = LinearSVC(C=sparsity_param, penalty="l1", dual=False).fit(X, y)
 	model = SelectFromModel(lsvc, prefit=True)
 	X_new = model.transform(X)
 	return X_new
+
+if __name__ == '__main__':
+	input_path = '0_train.csv'
+	k = 10
+	data, labels = load_data(input_path)
+	print len(data), len(labels)
+	# low_dim_data = univariate_fea_selection(data, labels, k)
+	low_dim_data = my_SelectFromModel(data, labels, 0.01)
+	print low_dim_data.shape
