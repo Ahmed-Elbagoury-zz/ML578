60302b4ee2dfe0e1c0e0b8576bf1b7eff49841ba
RaniaSalama <rania.ibrahim.salama@gmail.com>
2017-11-08 19:02:24 +0000

    fixing merge problem, adding run_all

---
+++ run_all.py
@@ -0,0 +1,5 @@
+from run import run
+#for i in range(17):
+#    run(i)
+for i in range(10, 17):
+    run(i)
--- select_c_for_SVM_using_kfold_CV.py
+++ select_c_for_SVM_using_kfold_CV.py
@@ -3,6 +3,7 @@ import os
 from kfold_cross_validation import kfold_cross_validation
 from plot_cross_validation_SVM import plot_cross_validation_SVM
 import random
+import numpy as np
 def select_c_for_SVM_using_kfold_CV(train_file, kernel, classification_alg,
                                     fea_selection_alg, c_vals, k,
                                     number_of_features_to_select):
@@ -16,19 +17,20 @@ def select_c_for_SVM_using_kfold_CV(train_file, kernel, classification_alg,
         for label in labels:
             output_file.write('%s\t' %(label))
         output_file.write('\n')
-        vals_to_plot = []
+        mean_vals = []
+        error_vals = []
         for c in c_vals:
             print '\tC = %f' %(c)
-            stats_vals = kfold_cross_validation(k, train_file, [fea_selection_alg, classification_alg], output_folder,
-                                               [number_of_features_to_select, c, kernel, 0])
+            stats_results = kfold_cross_validation(k, train_file, [fea_selection_alg, classification_alg], output_folder,
+                                                   [number_of_features_to_select, c, kernel, 0])
             output_file = open('%s/stats.txt' %(output_folder), 'a')
             output_file.write('%f\t' %(c))
-            vals_to_plot.append([])
-            for k in range(4):
-                mean = stats_vals[k][0]
-                var = stats_vals[k][1]
-                output_file.write('%f\t%f\t' %(mean, var))
-                vals_to_plot[-1].extend([mean, var])
+            mean_vals.append([])
+            error_vals.append([])
+            for stat_record in stats_results:
+                mean_vals[-1].append(stat_record[0])
+                error_vals[-1].append(stat_record[1])
+                output_file.write('%f\t%f\t' %(stat_record[0], stat_record[1]))
             output_file.write('\n')
             output_file.close()
-        plot_cross_validation_SVM(c_vals, labels, vals_to_plot, '%s/stats.png' %(output_folder), range(len(vals_to_plot)), False)
+        plot_cross_validation_SVM(k, c_vals, labels, np.array(mean_vals), np.array(error_vals), '%s/stats' %(output_folder), range(len(mean_vals)), False)
